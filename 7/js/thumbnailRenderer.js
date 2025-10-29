a52f81bb7a13dc00b9de4f6a61b2c11a10f08189
Keks <keks@htmlacademy.ru>
2025-10-29 11:53:38 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/thumbnailRenderer.js
@@ -0,0 +1,37 @@
+import {generatePhotoDescriptions} from './data.js';
+
+const pictures=document.querySelector('.pictures');
+const picture=document.querySelector('#picture').content.querySelector('.picture');
+
+const renderPhoto = (photo) => {
+  const {url, comments, likes, description} = photo;
+  const clonedElement = picture.cloneNode(true);
+  clonedElement.querySelector('.picture__likes').textContent = likes;
+  clonedElement.querySelector('.picture__comments').textContent = comments;
+  const img = clonedElement.querySelector('.picture__img');
+  img.src = url;
+  img.alt = description;
+
+  clonedElement.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    generatePhotoDescriptions(photo);
+  });
+
+  return clonedElement;
+};
+
+const fragment=document.createDocumentFragment();
+
+const clearPhotos=() =>{
+  pictures.innerHTML='';
+};
+
+const renderPhotos = (obj) => {
+  clearPhotos();
+  obj.forEach((i) => {
+    fragment.appendChild(renderPhoto(i));
+  });
+  pictures.appendChild(fragment);
+};
+
+renderPhotos();
