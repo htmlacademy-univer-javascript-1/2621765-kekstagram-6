78638b015b0ccdd8c74550349476f526e3de9570
Табельская Алиса <alya.tabelskaya@bk.ru>
2025-11-24 16:22:35 +0500

    готово 9.11

--- js/formHandler.js
+++ js/formHandler.js
@@ -1,45 +1,148 @@
-/*Изучите, что значит загрузка изображения, и как, когда и каким образом показывается форма редактирования изображения.
-Напишите код и добавьте необходимые обработчики для реализации этого пункта техзадания. В работе вы можете опираться на код показа
-окна с полноразмерной фотографией, который вы, возможно, уже написали в предыдущей домашней работе.*/
+const MAX_SYMBOLS=20;
+const MAX_COMMENT_SYMBOLS=140;
+const MAX_HASHTAGS=5;
 
-/*Выбор изображения для загрузки осуществляется с помощью стандартного контрола загрузки файла .img-upload__input, который
-стилизован под букву «О» в логотипе.
-После выбора изображения (изменения значения поля .img-upload__input), показывается форма редактирования изображения.
-У элемента .img-upload__overlay удаляется класс hidden, а body задаётся класс modal-open.
-
-После выбора изображения пользователем с помощью стандартного контрола загрузки файла .img-upload__input, нужно подставить
- его в форму редактирования вместо тестового изображения в блок предварительного просмотра и в превью эффектов.
-
-1.3 Закрытие формы редактирования изображения производится либо нажатием на кнопку .img-upload__cancel, либо нажатием клавиши
- Esc. Элементу .img-upload__overlay возвращается класс hidden. У элемента body удаляется класс modal-open.*/
-//import{isEscKey} from './util.js';
 const imgInput=document.querySelector('.img-upload__input');
 const imgOverlay=document.querySelector('.img-upload__overlay');
 const imgCancel=document.querySelector('.img-upload__cancel');
-
+//const imgPreview=document.querySelector('.img-upload__preview');
+const imgForm=document.querySelector('.img-upload__form');
+const textHashtags=document.querySelector('.text__hashtags');
+const imgSubmit=document.querySelector('.img-upload__submit');
+const comments=document.querySelector('.text__description');
 
 imgInput.addEventListener('change',()=>{
   imgOverlay.classList.remove('hidden');
   document.body.classList.add('modal-open');
-
 });
 
 
 imgCancel.addEventListener('click',()=>{
+  imgInput.value = '';
+  imgForm.reset();
   imgOverlay.classList.add('hidden');
   document.body.classList.remove('modal-open');
 });
 
-/*function formHandlerEscKeyDown(evt){
-  if (isEscKey(evt)){
-    imgOverlay.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-  }
-}*/
-imgCancel.addEventListener('keydown', (evt) => {
+document.addEventListener('keydown', (evt) => {
   if (evt.key === 'Escape') {
     evt.preventDefault();
+    imgInput.value = '';
+    imgForm.reset();
     imgOverlay.classList.add('hidden');
     document.body.classList.remove('modal-open');
   }
 });
+
+const pristine = new Pristine(imgForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'img-upload__item--invalid',
+  successClass: 'img-upload__item--valid',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__error',
+  errorTextTag: 'div',
+});
+
+let errorMessage='';
+
+const error=()=>errorMessage;
+
+const hashtagsHandler=(value)=>{
+  errorMessage='';
+
+  const inputText=value.toLowerCase().trim();
+
+  if (!inputText){
+    return true;
+  }
+
+  const inputArray=inputText.split(/\s+/);
+
+  const rules=[
+    {
+      check:inputArray.some((item)=>item.indexOf('#',1)>=1),
+      error:'Хэш-теги разделяются пробелами',
+    },
+    {
+      check:inputArray.some((item)=>item[0]!=='#'),
+      error:'Хэш-тег должен начинаться с символа #',
+    },
+    {
+      check:inputArray.some((item,num,arr)=>arr.includes(item,num+1)),
+      error:'Хэш-теги не должны повторяться',
+    },
+    {
+      check:inputArray.some((item)=>item.length > MAX_SYMBOLS),
+      error:`Максимальная длина одного хэш-тега ${MAX_SYMBOLS} символов, включая решётку`,
+    },
+    {
+      check:inputArray.length > MAX_HASHTAGS,
+      error:`Нелзя указать больше ${MAX_HASHTAGS} хэш-тегов`,
+    },
+    {
+      check:inputArray.some((item)=>!/^#[a-zа-яё0-9]{1,19}$/i.test(item)),
+      error:'Хэш-теш содержит недопустимые символы',
+    },
+  ];
+
+  return rules.every((rule)=>{
+    const isInvalid=rule.check;
+    if(isInvalid){
+      errorMessage=rule.error;
+    }
+    return !isInvalid;
+  });
+};
+
+const commentsHandler=(value)=>{
+  errorMessage='';
+  const inputText=value.toLowerCase().trim();
+
+  if (!inputText){
+    return true;
+  }
+  const inputArray=inputText.split(/\s+/);
+
+  const rules=[
+    {
+      check:inputArray.some((item)=>item.length > MAX_COMMENT_SYMBOLS),
+      error:`Максимальная длина комментария ${MAX_COMMENT_SYMBOLS} символов`,
+    },
+  ];
+
+  return rules.every((rule)=>{
+    const isInvalid=rule.check;
+    if(isInvalid){
+      errorMessage=rule.error;
+    }
+    return !isInvalid;
+  });
+};
+
+pristine.addValidator(comments,commentsHandler,error,1,false);
+pristine.addValidator(textHashtags,hashtagsHandler,error,2,false);
+
+const onContentInput=()=>{
+  if(pristine.validate()){
+    imgSubmit.disabled = false;
+  }else{
+    imgSubmit.disabled = true;
+  }
+};
+
+comments.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    evt.stopPropagation();
+  }
+});
+
+textHashtags.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    evt.stopPropagation();
+  }
+});
+
+
+comments.addEventListener('input',onContentInput);
+textHashtags.addEventListener('input',onContentInput);
+
